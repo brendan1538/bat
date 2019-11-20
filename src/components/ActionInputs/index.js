10bc1e21c3bbc7de5b5c3dfcde34d8e7caa05745
Brendan Rielly <brendan.rielly@endurance.com>
2019-11-20 17:56:54 -0500

    UI Updates

---
+++ src/components/ActionInputs/index.js
@@ -0,0 +1,128 @@
+import React from "react";
+import { Button, Form, Header } from "semantic-ui-react";
+
+const styles = {
+  select: {
+    fontFamily: "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
+    margin: "0",
+    outline: "0",
+    tapHighlightColor: "rgba(255,255,255,0)",
+    lineHeight: "1.21428571em",
+    padding: ".67857143em 1em",
+    fontSize: "1em",
+    background: "#fff",
+    border: "1px solid rgba(34,36,38,.15)",
+    color: "rgba(0,0,0,.87)",
+    borderRadius: ".28571429rem",
+    boxShadow: "0 0 0 0 transparent inset",
+    transition: "color .1s ease,border-color .1s ease",
+    flex: "1 0 auto",
+    height: "38px"
+  }
+};
+
+const ActionInputs = props => {
+  const { actions, handleDelete } = props;
+  return actions.map((action, index) => {
+    const nameId = `actionName${index}`;
+    const commandId = `actionCommand${index}`;
+    const argsId = `actionArgs${index}`;
+    const directoryId = `actionDirectory${index}`;
+    const actionTypeId = `actionType${index}`;
+
+    return (
+      <div className="action-inputs" key={`actionInput${index}`}>
+        <Header key={`header${index}`} content={`Action ${index}`}></Header>
+        <Form.Group key={`group1-${index}`} widths="equal">
+          <Form.Field>
+            <label htmlFor={actionTypeId}>Action Type</label>
+            <div className="ui fluid input">
+              <select
+                name={actionTypeId}
+                id={actionTypeId}
+                data-id={index}
+                defaultValue={action.type}
+                className="type"
+                style={styles.select}
+              >
+                <option value="custom">Custom</option>
+                <option value="docker">Docker</option>
+                <option value="git">Git</option>
+                <option value="url">Open URL</option>
+                <option value="sr">Create SR Box</option>
+              </select>
+            </div>
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor={nameId}>Name</label>
+            <div className="ui fluid input">
+              <input
+                type="text"
+                id={nameId}
+                data-id={index}
+                defaultValue={action.name}
+                name={nameId}
+                className="name"
+              />
+            </div>
+          </Form.Field>
+        </Form.Group>
+        <Form.Group key={`group2-${index}`} widths="equal">
+          <Form.Field>
+            <label htmlFor={commandId}>Command</label>
+            <div className="ui fluid input">
+              <input
+                type="text"
+                id={commandId}
+                data-id={index}
+                defaultValue={action.command}
+                name={commandId}
+                className="command"
+              />
+            </div>
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor={argsId}>Arguments</label>
+            <div className="ui fluid input">
+              <input
+                type="text"
+                id={argsId}
+                data-id={index}
+                defaultValue={action.args}
+                name={argsId}
+                className="args"
+              />
+            </div>
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor={directoryId}>Directory</label>
+            <div className="ui fluid input">
+              <input
+                type="text"
+                id={directoryId}
+                data-id={index}
+                defaultValue={action.directory}
+                name={directoryId}
+                className="directory"
+              />
+            </div>
+          </Form.Field>
+          <Form.Field>
+            <label>Remove Action</label>
+            <Button
+              type="button"
+              data-id={index}
+              icon
+              negative
+              onClick={handleDelete}
+            >
+              Delete
+            </Button>
+          </Form.Field>
+        </Form.Group>
+      </div>
+    );
+  });
+};
+
+export default ActionInputs;
---
+++ src/components/CreateBundleModal/index.js
@@ -0,0 +1,162 @@
+import React, { Component } from "react";
+import { Modal, Form, Button, Header } from "semantic-ui-react";
+import axios from "axios";
+
+import ActionInputs from "../ActionInputs";
+
+const styles = {
+  submit: {
+    padding: "20px 0 40px 0"
+  }
+};
+
+const sendNewBundle = bundle => {
+  console.log("Sending new Bundle to backend:", bundle);
+
+  axios
+    .post(`http://localhost:1538/createBundle/`, bundle)
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+};
+
+class CreateBundleModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      image: "",
+      description: "",
+      actions: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.removeAction = this.removeAction.bind(this);
+  }
+
+  updateActionState(index, name, value) {
+    let actions = [...this.state.actions];
+    actions[index][name] = value;
+
+    this.setState({ actions });
+  }
+
+  updateActionType(event) {
+    const target = event.target;
+    const value = target.value;
+
+    if (value === "docker") {
+      this.updateActionState(target.dataset.id, "command", "docker-compose up");
+    }
+    if (value === "git") {
+      this.updateActionState(target.dataset.id, "command", "git checkout");
+    }
+    if (value === "url") {
+      this.updateActionState(target.dataset.id, "command", "open ");
+    }
+    if (value === "sr") {
+      this.updateActionState(target.dataset.id, "command", "sr");
+    }
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    if (target.id.includes("action")) {
+      this.updateActionState(target.dataset.id, target.className, value);
+    } else {
+      this.setState({ [name]: target.value });
+    }
+
+    if (target.id.includes("type")) {
+      this.updateActionType(event);
+    }
+  }
+
+  handleSubmit = event => {
+    const { handleModal } = this.props;
+    const data = {
+      ...this.state
+    };
+
+    sendNewBundle(data);
+    handleModal();
+  };
+
+  addAction = event => {
+    this.setState(prevState => ({
+      actions: [
+        ...prevState.actions,
+        { type: "", name: "", command: "", args: "", directory: "" }
+      ]
+    }));
+  };
+
+  removeAction = event => {
+    const index = event.target.dataset.id;
+
+    this.setState(
+      prevState => ({
+        actions: prevState.actions.splice(index, 1)
+      }),
+      () => console.log(this.state)
+    );
+  };
+
+  render() {
+    const { name, image, description, actions } = this.state;
+    return (
+      <Modal.Content>
+        <Header>Create a New Bundle</Header>
+        <Modal.Description>
+          <Form onChange={this.handleChange} onSubmit={this.handleSubmit}>
+            <Form.Group widths="equal">
+              <Form.Input
+                label="Bundle Name"
+                placeholder="Bundle Name"
+                value={name}
+                name="name"
+              />
+              <Form.Input
+                label="Image URL"
+                placeholder="Image URL"
+                value={image}
+                name="image"
+              />
+            </Form.Group>
+            <Form.TextArea
+              label="Description"
+              placeholder="Description"
+              value={description}
+              name="description"
+            />
+            {actions && (
+              <ActionInputs
+                actions={actions}
+                handleDelete={this.removeAction}
+                handleChange={this.handleChange}
+              />
+            )}
+            <Form.Group>
+              <Button
+                primary
+                onClick={this.addAction}
+                content="Add an Action"
+                type="button"
+              />
+            </Form.Group>
+            <Form.Field style={styles.submit}>
+              <Button floated="right" type="submit" positive>
+                Create Bundle
+              </Button>
+            </Form.Field>
+          </Form>
+        </Modal.Description>
+      </Modal.Content>
+    );
+  }
+}
+
+export default CreateBundleModal;
