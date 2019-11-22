3951c05d7a114e74fd4fb81248fef3f225a64c82
Brendan Laughlin <brendan.laughlin@endurance.com>
2019-11-22 10:21:11 -0500

    updated electron window size

--- public/main.js
+++ public/main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 600,
-    height: 500,
+    width: 1500,
+    height: 800,
     webPreferences: {
       nodeIntegration: true,
     },
